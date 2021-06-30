1a450304408e39075f52d3465a631647b075c141
Антон Коньков <askonkov_1@edu.hse.ru>
2021-06-30 23:28:34 +0500

    Общие интерфейс для всех ответов

---
+++ RodionLIbrary/IAnswer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodionLIbrary
+{
+    public interface IAnswer
+    {
+        string SolutionText { get; }
+    }
+}
