b9e102021362a7761197803c7d38e1a420df4c48
Антон Коньков <askonkov_1@edu.hse.ru>
2021-06-30 23:41:54 +0500

    Добавлен доверительный интервал для мат.ожидания

---
+++ RodionLIbrary/Confidence_Intervals/MathExpectation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodionLIbrary.Confidence_Intervals
+{
+    public class MathExpectation : IConfidenceInterval
+    {
+        private double _confidenceLevel;
+
+        public MathExpectation(double confidenceLevel, bool isNormalDistribution, bool isSdKnown, double mean, double sd)
+        {
+            ConfidenceLevel = confidenceLevel;
+        }
+
+        public MathExpectation(double confidenceLevel, bool isNormalDistribution, bool isSdKnown, IEnumerable<double> data)
+        {
+            ConfidenceLevel = confidenceLevel;
+        }
+
+        public double ConfidenceLevel 
+        {
+            get => _confidenceLevel;
+
+            set
+            {
+                if (value < 1 || value > 100) throw new Exception("Confidence level is out of range (1-100%)");
+
+                _confidenceLevel = value;
+            } 
+        }
+
+        public IAnswer GetDoubleSided()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAnswer GetLeftSided()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAnswer GetRightSided()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
