06fba5edb41b6e8df010ff442b097a48c53a9437
Антон Коньков <askonkov_1@edu.hse.ru>
2021-06-30 23:29:01 +0500

    Добавлен интерефейс для интервалов

---
+++ RodionLIbrary/Confidence_Intervals/IConfidenceInterval.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodionLIbrary.Confidence_Intervals
+{
+    public interface IConfidenceInterval
+    {
+        double ConfidenceLevel { get; set; }
+
+        IAnswer GetLeftSided();
+
+        IAnswer GetRightSided();
+
+        IAnswer GetDoubleSided();
+    }
+}
