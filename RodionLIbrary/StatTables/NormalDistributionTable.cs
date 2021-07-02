4bb21ee06068685eb6f03d8679088306051cff76
Антон Коньков <askonkov_1@edu.hse.ru>
2021-07-02 13:44:44 +0500

    Добавлен класс для таблицы стандартного нормального распределения

---
+++ RodionLIbrary/StatTables/NormalDistributionTable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodionLIbrary.StatTables
+{
+    public class NormalDistributionTable
+    {
+        public double GetT(int confidenceLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetX(int confidenceLevel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
