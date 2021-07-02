93da1fb25850f9eb0f9dcb1499ccaf6ef4b0ce21
Антон Коньков <askonkov_1@edu.hse.ru>
2021-07-02 12:59:08 +0500

    Добавлен класс для интервала мат.ожидания, если распределение нормальное и известно стандартное отклонение

---
+++ RodionLIbrary/Confidence_Intervals/MathExpectationSdKnown.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodionLIbrary.Confidence_Intervals
+{
+    public class MathExpectationSdKnown : MathExpectation
+    {
+        public MathExpectationSdKnown(int confidenceLevel, double sd, int n, double mean) : base(confidenceLevel, n, mean) 
+        {
+            Sd = sd;
+        }
+
+        public MathExpectationSdKnown(int confidenceLevel, double sd, IEnumerable<double> data) : base(confidenceLevel, data) 
+        {
+            Sd = sd;
+        }
+
+        public double Sd { get; set; }
+
+        public override IAnswer GetDoubleSided()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IAnswer GetLeftSided()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IAnswer GetRightSided()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
