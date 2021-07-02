0ff3603ed4f8b7d425e93252f09f566392655dc8
Антон Коньков <askonkov_1@edu.hse.ru>
2021-07-02 12:54:42 +0500

    Добавлен класс для интервалов мат.ожидания, если рапределение не нормальное

---
+++ RodionLIbrary/Confidence_Intervals/MathExpectationNotNormalDistribution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodionLIbrary.Confidence_Intervals
+{
+    public class MathExpectationNotNormalDistribution : MathExpectation
+    {
+        public MathExpectationNotNormalDistribution(int confidenceLevel, int n, double mean) : base(confidenceLevel, n, mean) { }
+
+        public MathExpectationNotNormalDistribution(int confidenceLevel, IEnumerable<double> data) : base(confidenceLevel, data) { }
+
+        public override IAnswer GetDoubleSided()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IAnswer GetLeftSided()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IAnswer GetRightSided()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
