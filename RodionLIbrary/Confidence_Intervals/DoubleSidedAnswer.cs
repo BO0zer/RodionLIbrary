db1fe04cb5022c6c28a341bb3136b561ea8a53c7
Антон Коньков <askonkov_1@edu.hse.ru>
2021-07-02 14:25:20 +0500

    Класс для ответа на двусторонний доверительный интервал

---
+++ RodionLIbrary/Confidence_Intervals/DoubleSidedAnswer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodionLIbrary.Confidence_Intervals
+{
+    public class DoubleSidedAnswer : IAnswer
+    {
+        private const int _defaultDecimals = 3;
+
+        public DoubleSidedAnswer(double leftBorder, double rightBorder)
+        {
+            RightBorder = rightBorder;
+            LeftBorder = leftBorder;
+            Difference = RightBorder - LeftBorder;
+
+            double left = Round(LeftBorder), right = Round(RightBorder);
+
+            SolutionText = $"Двусторонний довирительный интервал: {left} <= a <= {right}";
+            SolutionText += $"\nЛевая граница: {left}";
+            SolutionText += $"\nПравая граница: {right}";
+            SolutionText += $"\nРазница: {Round(Difference)}";
+        }
+
+        public string SolutionText { get; private set; }
+
+        public double LeftBorder { get; private set; }
+
+        public double RightBorder { get; private set; }
+
+        public double Difference { get; private set; }
+
+        private static double Round(double value, int decimals = _defaultDecimals) => Math.Round(value, decimals);
+    }
+}
