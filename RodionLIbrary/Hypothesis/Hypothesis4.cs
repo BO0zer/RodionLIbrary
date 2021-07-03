ef9911a7207f4136af03c05ad3652be753edbe6d
BO0zer <yurgroshev@edu.hse.ru>
2021-07-03 14:17:30 +0500

    Реалихован класс Hypothesis4

--- RodionLIbrary/Hypothesis/Hypothesis4.cs
+++ RodionLIbrary/Hypothesis/Hypothesis4.cs
@@ -9,7 +9,24 @@ namespace RodionLIbrary.Hypothesis
     /// <summary>
     /// Гипотеза для вероятности (w = m / n)
     /// </summary>
-    class Hypothesis4
+    class Hypothesis4:Answer
     {
+        /// <summary>
+        /// Конструктор для создания экземпляра класса Hypothesis4 (каждый экземпляр представляет собой задачу, связанную с данной гипотезой)
+        /// </summary>
+        /// <param name="count">Количество объектов</param>
+        /// <param name="m">Количество объектов, способствующие успеху прямопропорционально </param>
+        /// <param name="p">Предложенная вероятность успеха</param>
+        /// <param name="q">Предложенная вероятность неудачи (1-p)</param>
+        public Hypothesis4(double count, double m, double p, double q)
+        {
+            var w = m / count;
+            var u = Round((w - p) / Math.Sqrt(p * q / count));
+            SolutionText = "Вероятность успеха (w) = Количество объектов, способствующие успеху прямопропорционально (m) / Количество объектов (count)";
+            AddText($"w = {m} / {count}");
+            AddText("U(выч) = (Вероятность успеха (w) - p) / кв. корень(p * q / count)");
+            AddText($"U(выч) = ({w} - {p}) / кв. корень({p} * {q} / {count})");
+            AddText($"U(выч) = {u}");
+        }
     }
 }
