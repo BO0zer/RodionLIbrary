500dd4d6985922c590ecbd5f1fcba5127ac67c13
BO0zer <yurgroshev@edu.hse.ru>
2021-07-03 15:04:26 +0500

    Переимненована первая гипотеза на более человечное название, исправлены ее недочеты

--- RodionLIbrary/Hypothesis/HypothesisSKnown.cs
+++ RodionLIbrary/Hypothesis/HypothesisSKnown.cs
@@ -9,20 +9,20 @@ namespace RodionLIbrary.Hypothesis
     /// <summary>
     /// Нормальное распределение сигма^2 известно, a=a0
     /// </summary>
-    class Hypothesis1 : Answer
+    class HypothesisSKnown : Answer
     {
         /// <summary>
         /// Конструктор для создания экземпляра класса Hypothesis1 (каждый экземпляр представляет собой задачу, связанную с данной гипотезой)
         /// </summary>
         /// <param name="mean">Среднее значение совокупности</param>
         /// <param name="count">Количество элементов в совокупности</param>
-        /// <param name="averageSd">Среднее квадратичное отклонение</param>
+        /// <param name="sd">Стандартное отклонение</param>
         /// <param name="a">Математическое ожидание</param>
-        public Hypothesis1(double mean, double count, double averageSd, double a)
+        public HypothesisSKnown(double mean, double count, double sd, double a)
         {
-            var u = Round((mean - a) / (averageSd / Math.Sqrt(count)));
-            SolutionText = "U(выч)= (ср. значение совокупности - a) / (ср. кв. откл. / кв. корень(кол-во элементов в совокупности))";
-            AddText($"U(выч)= ({mean} - {a}) / ({averageSd} / кв. корень({count}))");
+            var u = Round((mean - a) / (sd / Math.Sqrt(count)));
+            SolutionText = "U(выч)= (X(ср) - a) / (sd / кв. корень(n))";
+            AddText($"U(выч)= ({mean} - {a}) / ({sd} / кв. корень({count}))");
             AddText($"U(выч)= {u}");
         }
     }
