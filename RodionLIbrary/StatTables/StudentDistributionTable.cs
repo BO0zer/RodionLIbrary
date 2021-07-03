1250d672c2c55d210d01a45912c2e727b378d38f
Антон Коньков <askonkov_1@edu.hse.ru>
2021-07-03 11:53:20 +0500

    Добален класс для таблицы Стьюдента

---
+++ RodionLIbrary/StatTables/StudentDistributionTable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodionLIbrary.StatTables
+{
+    public abstract class StudentDistributionTable
+    {
+        public static double GetT(int confidenceLevel, int degree)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double GetX(int confidenceLevel, int degree)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
