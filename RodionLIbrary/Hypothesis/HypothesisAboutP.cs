417b2c7c8ab20de09f509273c80c1f0e016f961c
BO0zer <yurgroshev@edu.hse.ru>
2021-07-03 15:05:37 +0500

    Переимненована четвертая гипотеза на более человечное название, исправлены ее мелкие недочеты

---
+++ RodionLIbrary/Hypothesis/HypothesisAboutP.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodionLIbrary.Hypothesis
+{
+    /// <summary>
+    /// Гипотеза для вероятности (w = m / n)
+    /// </summary>
+    class HypothesisAboutP:Answer
+    {
+        /// <summary>
+        /// Конструктор для создания экземпляра класса Hypothesis4 (каждый экземпляр представляет собой задачу, связанную с данной гипотезой)
+        /// </summary>
+        /// <param name="count">Количество объектов</param>
+        /// <param name="m">Количество объектов, способствующие успеху прямопропорционально </param>
+        /// <param name="p">Предложенная вероятность успеха</param>
+        /// <param name="q">Предложенная вероятность неудачи (1-p)</param>
+        public HypothesisAboutP(double count, double m, double p, double q)
+        {
+            var w = m / count;
+            var u = Round((w - p) / Math.Sqrt(p * q / count));
+            SolutionText = "w =  m / count";
+            AddText($"w = {m} / {count}");
+            AddText("U(выч) = (w - p) / кв. корень(p * q / count)");
+            AddText($"U(выч) = ({w} - {p}) / кв. корень({p} * {q} / {count})");
+            AddText($"U(выч) = {u}");
+        }
+    }
+}
