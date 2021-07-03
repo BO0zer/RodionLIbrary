fb9dd2cc46d0ba137460e933b68a670c0cb27926
BO0zer <yurgroshev@edu.hse.ru>
2021-07-03 15:05:24 +0500

    Переимненована третья гипотеза на более человечное название, исправлены ее мелкие недочеты

--- RodionLIbrary/Hypothesis/HypothesisAboutS.cs
+++ RodionLIbrary/Hypothesis/HypothesisAboutS.cs
@@ -9,18 +9,18 @@ namespace RodionLIbrary.Hypothesis
     /// <summary>
     /// Нормальное распределение a неизвестно, сигма^2=сигма^2
     /// </summary>
-    class Hypothesis3: Answer
+    class HypothesisAboutS: Answer
     {
         /// <summary>
         /// Конструктор для создания экземпляра класса Hypothesis3 (каждый экземпляр представляет собой задачу, связанную с данной гипотезой)
         /// </summary>
         /// <param name="count">Количество элементов в совокупности</param>
         /// <param name="s">Выборочная дисперсия</param>
-        /// <param name="s0">Математическое ожидание</param>
-        public Hypothesis3(double count, double s, double s0)
+        /// <param name="s0">Дисперсия</param>
+        public HypothesisAboutS(double count, double s, double s0)
         {
             var u = Round(count * s / s0);
-            SolutionText = "U(выч)= Кол-во элементов в совокупности  * Дисперсия / Мат. ожидание";
+            SolutionText = "U(выч)= n * s^2 / sd^2";
             AddText($"U(выч)= {count} * {s} / {s0}");
             AddText($"U(выч)= {u}");
         }
