ceb30609d72489140771f7e6a6e725b4e76b2bdd
Антон Коньков <askonkov_1@edu.hse.ru>
2021-07-03 12:30:35 +0500

    Добавлен для для доверительного интервала дисперсии

---
+++ RodionLIbrary/Confidence_Intervals/Variance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodionLIbrary.Confidence_Intervals
+{
+    public class Variance : IConfidenceInterval
+    {
+        public int ConfidenceLevel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public IAnswer GetDoubleSided()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAnswer GetLeftSided()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAnswer GetRightSided()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
