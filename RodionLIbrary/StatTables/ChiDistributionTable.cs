dd261f679b61b6a2e731d1f921d71fffff76fe73
Антон Коньков <askonkov_1@edu.hse.ru>
2021-07-03 12:45:06 +0500

    Добавлен класс для статистической таблицы хи-квадрат

---
+++ RodionLIbrary/StatTables/ChiDistributionTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodionLIbrary.StatTables
+{
+    public class ChiDistributionTable
+    {
+        public static double GetX(int confidenceLevel, int degree)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
