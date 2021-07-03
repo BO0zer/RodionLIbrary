1f3d52ecec5c11524a6d540f14c518315d5c8c11
BO0zer <yurgroshev@edu.hse.ru>
2021-07-03 14:17:14 +0500

    Реализован класс Hupothesis3

--- RodionLIbrary/Hypothesis/Hypothesis3.cs
+++ RodionLIbrary/Hypothesis/Hypothesis3.cs
@@ -9,7 +9,20 @@ namespace RodionLIbrary.Hypothesis
     /// <summary>
     /// Нормальное распределение a неизвестно, сигма^2=сигма^2
     /// </summary>
-    class Hypothesis3
+    class Hypothesis3: Answer
     {
+        /// <summary>
+        /// Конструктор для создания экземпляра класса Hypothesis3 (каждый экземпляр представляет собой задачу, связанную с данной гипотезой)
+        /// </summary>
+        /// <param name="count">Количество элементов в совокупности</param>
+        /// <param name="s">Выборочная дисперсия</param>
+        /// <param name="s0">Математическое ожидание</param>
+        public Hypothesis3(double count, double s, double s0)
+        {
+            var u = Round(count * s / s0);
+            SolutionText = "U(выч)= Кол-во элементов в совокупности  * Дисперсия / Мат. ожидание";
+            AddText($"U(выч)= {count} * {s} / {s0}");
+            AddText($"U(выч)= {u}");
+        }
     }
 }
