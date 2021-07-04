9d13705b2c8e2119d0e8dc42f5d0bbb22c084eac
Антон Коньков <askonkov_1@edu.hse.ru>
2021-07-04 16:00:16 +0500

    Реализован коэффициент корреляции Пирсона

---
+++ RodionLIbrary/Correlation/PirsonCor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodionLIbrary.Correlation
+{
+    public abstract class PirsonCor
+    {
+        public static PirsonCorAnswer GetCor(double meanX, double meanY, double meanXY, double meanX2, double meanY2)
+        {
+            double sdX = Math.Sqrt(meanX2 - Math.Pow(meanX, 2));
+            double sdY = Math.Sqrt(meanY2 - Math.Pow(meanY, 2));
+            double cov = meanXY - meanX * meanY;
+            double cor = cov / (sdX * sdY);
+
+            string formula = "Sx = √(X^2 - (X\x0305)^2); Sy = √(Y^2 - (Y\x0305)^2); K*(X,Y) = XY - X*Y; r*(X,Y) = K*(X,Y) / (Sx * Sy)";
+            string calculation = $"Sx = √({meanX2} - {meanX}^2) = {sdX}; Sy = √({meanY2} - {meanY}^2) = {sdY}; K*(X,Y) = {meanXY} - {meanX}*{meanY} = {cov}; r*(X,Y) = {cov} / ({sdX} * {sdY}) = {cor}";
+
+            return new PirsonCorAnswer(cor, formula, calculation);
+        }
+    }
+}
---
+++ RodionLIbrary/Correlation/PirsonCorAnswer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodionLIbrary.Correlation
+{
+    public class PirsonCorAnswer : Answer
+    {
+        public PirsonCorAnswer(double cor, string formula, string calculation)
+        {
+            Cor = cor;
+
+            SolutionText = "Выборочный коэффициент корреляции:";
+            AddText(formula);
+            AddText(calculation);
+            AddText($"Выборочный коэффициент корреляции = {Round(Cor)}");
+        }
+
+        public double Cor { get; private set; }
+    }
+}
