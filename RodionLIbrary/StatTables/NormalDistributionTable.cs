47dcba240cf1c96a2e6eb8caf3e785e8e25a74f9
BO0zer <yurgroshev@edu.hse.ru>
2021-07-04 16:46:06 +0500

    int на double изменен для избежания багов при делении

--- RodionLIbrary/StatTables/NormalDistributionTable.cs
+++ RodionLIbrary/StatTables/NormalDistributionTable.cs
@@ -16,8 +16,8 @@ namespace RodionLIbrary.StatTables
             [95] = 1.64,
             [90] = 1.28
         };
-        public static double GetT(int confidenceLevel) => normalDistributionTable[(1 + confidenceLevel) / 2];
+        public static double GetT(double confidenceLevel) => normalDistributionTable[(1 + confidenceLevel) / 2];
 
-        public static double GetX(int confidenceLevel) => normalDistributionTable[confidenceLevel];
+        public static double GetX(double confidenceLevel) => normalDistributionTable[confidenceLevel];
     }
 }
