61a61a0959fa4463ad9b1b138385cb904eff1623
BO0zer <yurgroshev@edu.hse.ru>
2021-07-04 17:08:10 +0500

    Переделаны проценты на дробь

--- RodionLIbrary/StatTables/NormalDistributionTable.cs
+++ RodionLIbrary/StatTables/NormalDistributionTable.cs
@@ -10,11 +10,11 @@ namespace RodionLIbrary.StatTables
     {
         private static Dictionary<double, double> normalDistributionTable = new Dictionary<double, double>
         {
-            [99] = 2.33,
-            [98] = 2.05,
-            [97.5] = 1.96,
-            [95] = 1.64,
-            [90] = 1.28
+            [0.99] = 2.33,
+            [0.98] = 2.05,
+            [0.975] = 1.96,
+            [0.95] = 1.64,
+            [0.90] = 1.28
         };
         public static double GetT(double confidenceLevel) => normalDistributionTable[(1 + confidenceLevel) / 2];
 
