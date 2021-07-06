42682b91cba4062a2ebc47976261f3b426278efa
BO0zer <yurgroshev@edu.hse.ru>
2021-07-06 20:29:38 +0500

    Гипотеза для r(s)

---
+++ RodionLIbrary/Hypothesis/HypothesisAboutRs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodionLIbrary.Hypothesis
+{
+    class HypothesisAboutRs:Answer
+    {
+        public HypothesisAboutRs(double rs, int n)
+        {
+            var u = Round(rs * Math.Sqrt(n - 1));
+            SolutionText = "U(выч)= r * Корень кв.(n - 1)";
+            AddText($"U(выч)= {rs} * корень кв.({n - 1})");
+            AddText($"U(выч)= {u}");
+        }
+    }
+}
