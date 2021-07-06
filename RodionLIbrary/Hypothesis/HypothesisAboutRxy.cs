c376608091675cbebbf0f720165e3c9156fc934a
BO0zer <yurgroshev@edu.hse.ru>
2021-07-06 20:29:50 +0500

    Гипотеза для r(x, y)

---
+++ RodionLIbrary/Hypothesis/HypothesisAboutRxy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodionLIbrary.Hypothesis
+{
+    class HypothesisAboutRxy:Answer
+    {
+        public HypothesisAboutRxy(double rxy, int n)
+        {
+            var u = Round(rxy * Math.Sqrt(n - 2) / Math.Sqrt(1 - Math.Pow(rxy, 2)));
+            SolutionText = "U(выч)= r * Корень кв.(n - 2) / Корень кв.(1 - r^2)";
+            AddText($"U(выч)= {rxy} * Корень кв.({n - 2}) / Корень кв.({1 - rxy}^2)");
+            AddText($"U(выч)= {u}");
+        }
+    }
+}
